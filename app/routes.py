487b56439d9b8d0512c957ba1db8366c0f658498
averma1 <anika_verma@yahoo.com>
2019-08-01 10:51:52 -0400

    changed color of tab on click

--- app/routes.py
+++ app/routes.py
@@ -4,20 +4,20 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('welcome.html', title='RiskFDA')
+    return render_template('welcome.html', title='RiskFDA', tabisactive='activetab')
 
 @app.route('/recalls')
 def recalls():
-    return render_template('recalls.html', title='RiskFDA')
+    return render_template('recalls.html', title='RiskFDA', tabisactive='activetab')
 
 @app.route('/advevents')
 def advevents():
-    return render_template('adverse-events.html', title='RiskFDA')
+    return render_template('adverse-events.html', title='RiskFDA', tabisactive='activetab')
 
 @app.route('/faq')
 def faq():
-    return render_template('faq.html', title='RiskFDA')
+    return render_template('faq.html', title='RiskFDA', tabisactive='activetab')
 
 @app.route('/risk')
 def risk():
-    return render_template('risk.html', title='RiskFDA')
+    return render_template('risk.html', title='RiskFDA', tabisactive='activetab')
